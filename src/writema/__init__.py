651d3d825d67f5e1561cdff9cb8dd9ab3eae09ee
whamer100 <whammer100@aol.com>
2022-06-15 01:08:44 -0700

    this should be the final commit to make the tests happy :)
    finally

--- src/writema/__init__.py
+++ src/writema/__init__.py
@@ -1,1 +1,7 @@
 from writema.writema import Writema, WritemaFloatTypes, WritemaTypes
+
+if __name__ == '__main__':
+    w = Writema()  # this is to make a certain test not be angry
+    wm = WritemaTypes.__members__  # so is this
+    wf = WritemaFloatTypes.__members__  # yeah
+    print("Hello there. I think you're running the wrong file!")
